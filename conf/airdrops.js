124aea5ffb8fdf0eefcb7e4c7e9f5c9bf50b43b5
Luke Williams <luke@iquidus.io>
2019-01-04 21:42:17 +0100

    add airdrop 4

--- conf/airdrops.js
+++ conf/airdrops.js
@@ -15,6 +15,11 @@ var airdrops = [
     title: 'Airdrop #3',
     contract: '0x1cac28619f78cdf8da22f9cf523f995d3fe7e98c',
     multiplier: 3
+  },
+  {
+    title: 'Airdrop #4',
+    contract: '0x0ab4ab630841f93ba2cc588db4a7e646a38b58ed',
+    multiplier: 2
   }
 ];
 
